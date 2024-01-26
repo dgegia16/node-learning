3cef0d2e321827ca03a318fcf6fe9ec6f287a75d
daati35 <125276422+dgegia16@users.noreply.github.com>
2024-01-26 15:27:48 +0400

    added routing

--- first-section/index.js
+++ first-section/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -25,12 +26,32 @@ const http = require("http");
 
 // console.log("Will read file");
 
-// SERVER
+// SERVER, ROUTING
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("Overview Page!");
+  } else if (pathName === "/product") {
+    res.end("Product Page");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
-server.listen(8000, "127.0.0.1", () =>{
-    console.log('Listening to requests on port 8000');
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
